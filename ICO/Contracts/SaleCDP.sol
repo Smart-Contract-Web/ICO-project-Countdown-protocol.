c71df8ef761f68a55a63db6f5645d66604f33a9b
Fan Tai <35626820+Smart-Contract-Web@users.noreply.github.com>
2018-09-09 22:23:36 +0300

    Update SaleCDP.sol

--- ICO/Contracts/SaleCDP.sol
+++ ICO/Contracts/SaleCDP.sol
@@ -78,10 +78,7 @@ contract Ownable {
     _transferOwnership(_newOwner);
   }
 
-  /**
-   * @dev Transfers control of the contract to a newOwner.
-   * @param _newOwner The address to transfer ownership to.
-   */
+  
   function _transferOwnership(address _newOwner) internal {
     require(_newOwner != address(0));
     emit OwnershipTransferred(owner, _newOwner);
